74e48e78fc672e5296b07e4bce692b9b45842516
liviafort <livia.fortunato@academico.ifpb.edu.br>
2025-08-09 23:12:28 -0300

    MPU

--- components/mpu6050/include/mpu6050.h
+++ components/mpu6050/include/mpu6050.h
@@ -2,7 +2,7 @@
 #define MPU6050_H
 
 #include "driver/i2c.h"
-#include "esp_err.h"
+#include "esp_log.h"
 
 #define MPU6050_I2C_ADDRESS         0x68
 #define MPU6050_PWR_MGMT_1          0x6B
@@ -15,12 +15,12 @@ typedef struct {
     float gyro_offset_x;
     float accel_offset_x;
     float accel_offset_y;
-    bool calibrated;
-} mpu6050_calibration_t;
+    bool gyro_calibrated;
+} mpu6050_dev_t;
 
-esp_err_t mpu6050_init(i2c_port_t i2c_num);
-void mpu6050_calibrate_improved(mpu6050_calibration_t calib);
-esp_err_t mpu6050_read_gyro(floatgyro_x, float gyro_y, floatgyro_z, mpu6050_calibration_t calib);
-esp_err_t mpu6050_read_accel(floataccel_x, float accel_y, floataccel_z, mpu6050_calibration_t *calib);
+esp_err_t mpu6050_init(mpu6050_dev_t *dev);
+void mpu6050_calibrate_improved(mpu6050_dev_t *dev);
+esp_err_t mpu6050_read_gyro(mpu6050_dev_t *dev, float *gyro_x, float *gyro_y, float *gyro_z);
+esp_err_t mpu6050_read_accel(mpu6050_dev_t *dev, float *accel_x, float *accel_y, float *accel_z);
 
 #endif
